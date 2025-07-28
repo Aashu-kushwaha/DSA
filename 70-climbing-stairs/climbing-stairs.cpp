aec1bc9722d9a2a5f7d8a83d835433cc361f3a0e
Aashu-kushwaha <167160987+Aashu-kushwaha@users.noreply.github.com>
2025-07-28 11:30:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (73.22%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=3){
+            return n;
+        }
+        int a=2,b=3,c;
+        for(int i=4;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return b;
+    }
+};
