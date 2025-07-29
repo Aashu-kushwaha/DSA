5d817357d5371966b1877723ac97dced555a9b36
Aashu-kushwaha <167160987+Aashu-kushwaha@users.noreply.github.com>
2025-07-29 09:33:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (73.21%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n<=3){
+        if(n<3){
             return n;
         }
-        int a=2,b=3,c;
-        for(int i=4;i<=n;i++){
+        int a=1,b=2,c;
+        for(int i=3;i<=n;i++){
             c=a+b;
             a=b;
             b=c;
