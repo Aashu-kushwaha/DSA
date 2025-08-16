c104e8cc2f9872dad15b75e633003121a80029c0
Aashu-kushwaha <167160987+Aashu-kushwaha@users.noreply.github.com>
2025-08-16 23:33:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (68.88%) - LeetSync

--- 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.cpp
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.cpp
@@ -1,57 +1,38 @@
-class Solution 
-{
+class Solution {
 public:
-    int maxDiff(int num) 
-    {
+    int maxDiff(int num) {
         string s = to_string(num);
-        string max_s = s;
-        string min_s = s;
+      string max= s,min=s;
+      char toReplaceMax ='*';
 
-        for (char c : s) 
-        {
-            if (c != '9') 
-            {
-              for (char &ch : max_s) 
-                {
-                    if (ch == c)
-                        ch = '9';
-                }
-                break; 
+      for(char ch :s){
+        if(ch!='9'){
+            toReplaceMax=ch;
+            break;
+        }
+      }  
+      if(toReplaceMax!='*'){
+        replace(max.begin(),max.end(),toReplaceMax,'9');
+      }
+       char x='*';
+       char y='*';
+
+       if(s[0]!='1'){
+        x=s[0];
+        y='1';
+       }
+       else{
+        for(int i=0;i<s.size();i++){
+            if(s[i]!='0' && s[i]!='1'){
+                x=s[i];
+                y='0';
+                break;
             }
         }
-        if (s[0] != '1') 
-        {
-            char to_replace = s[0];
-            for (char &ch : min_s) 
-            {
-                if (ch == to_replace)
-                {
-                    ch = '1';
-                }
-            }
-        } 
-        else 
-        {
-            for (int i = 1; i < s.size(); i++) 
-            {
-                if (s[i] != '0' && s[i] != '1') 
-                {
-                    char to_replace = s[i];
-                    for (char &ch : min_s) 
-                    {
-                        if (ch == to_replace)
-                        {
-                            ch = '0';
-                        }
-                    }
-                    break;
-                }
-            }
-        }
-
-        int max_val = stoi(max_s);
-        int min_val = stoi(min_s);
-
-        return max_val - min_val;
+       }
+      if(x!='*'){
+        replace(min.begin(),min.end(),x,y);
+      }
+    return stoi(max)-stoi(min);
     }
 };
