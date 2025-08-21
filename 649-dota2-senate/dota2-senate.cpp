7e5379a8184341a623e8d7ddfc9d07c2c37f05c5
Aashu-kushwaha <167160987+Aashu-kushwaha@users.noreply.github.com>
2025-08-21 23:53:06 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (75.59%) - LeetSync

--- 649-dota2-senate/dota2-senate.cpp
+++ 649-dota2-senate/dota2-senate.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
-    bool removeSenator(string &senate,char ch,int idx){
-        bool checkLeftRemovalSide = false;
-        while(true){
-            if(idx==0){
-                checkLeftRemovalSide = true;
+    string predictPartyVictory(string str) {
+        queue<int> q1,q2;
+        int n=str.size();
+        for(int i=0;i<n;i++){
+            if(str[i]=='R'){
+                q2.push(i);
             }
-            if(senate[idx]==ch){
-                senate.erase(senate.begin()+idx);
-                break;
-            }
-            idx = (idx+1) % senate.length();
+            else q1.push(i);
         }
-        return checkLeftRemovalSide;
-    }
-
-    string predictPartyVictory(string senate) {
-        int R = count(senate.begin(),senate.end(),'R'); 
-        int D = senate.length() - R;
-        int idx = 0;
-
-        while(R > 0 && D > 0) {
-            if(senate[idx] == 'R'){
-                bool checkLeftRemovalSide = removeSenator(senate,'D',(idx+1)%senate.length());
-                D--;
-                if(checkLeftRemovalSide){
-                    idx--;
-                }
-            } else { 
-                bool checkLeftRemovalSide = removeSenator(senate,'R',(idx+1)%senate.length());
-                R--;
-                if(checkLeftRemovalSide){
-                    idx--;
-                }
+        while(!q1.empty() && !q2.empty()){
+            int a=q1.front();
+            int b=q2.front();
+            if(a<b){
+                q1.pop();
+                q2.pop();
+                q1.push(n);
+                n++;
             }
-            idx = (idx+1) % senate.length(); 
+            else {
+                  q1.pop();
+                q2.pop();
+                q2.push(n);
+                n++;
+            }
         }
-        return R==0 ? "Dire" : "Radiant";
+        if(q1.empty())return "Radiant";
+        else return "Dire";
     }
 };
