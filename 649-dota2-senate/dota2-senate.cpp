32a84d950dea2a56da9410696cdb3aa066a82b59
Aashu-kushwaha <167160987+Aashu-kushwaha@users.noreply.github.com>
2025-08-23 10:15:17 +0530

    Time: 1 ms (78.68%) | Memory: 11.5 MB (51.81%) - LeetSync

--- 649-dota2-senate/dota2-senate.cpp
+++ 649-dota2-senate/dota2-senate.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    string predictPartyVictory(string str) {
-        queue<int> q1,q2;
-        int n=str.size();
-        for(int i=0;i<n;i++){
-            if(str[i]=='R'){
-                q2.push(i);
-            }
-            else q1.push(i);
+    string predictPartyVictory(string senate) {
+      queue<int>rq,dq;
+      int n= senate.size();
+      for(int i=0;i<n;i++){
+        if(senate[i]=='R'){
+            rq.push(i);
         }
-        while(!q1.empty() && !q2.empty()){
-            int a=q1.front();
-            int b=q2.front();
-            if(a<b){
-                q1.pop();
-                q2.pop();
-                q1.push(n);
-                n++;
+        else{
+            dq.push(i);
+        }
+      }
+        while(!rq.empty()&& !dq.empty()){
+            int ri= rq.front();
+            rq.pop();
+
+            int di= dq.front();
+            dq.pop();
+
+            if(ri<di){
+                rq.push(ri+n);
             }
-            else {
-                  q1.pop();
-                q2.pop();
-                q2.push(n);
-                n++;
+            else{
+                dq.push(di+n);
             }
         }
-        if(q1.empty())return "Radiant";
-        else return "Dire";
+      
+        return (rq.size()>dq.size())?"Radiant":"Dire";
     }
 };
