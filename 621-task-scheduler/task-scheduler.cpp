30e1a1ec49261a617061583f1012b606cd180588
Aashu-kushwaha <167160987+Aashu-kushwaha@users.noreply.github.com>
2025-08-28 23:42:07 +0530

    Time: 0 ms (100.00%) | Memory: 38.1 MB (99.60%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+       vector<int>mp(26,0);
+       for(char &ch :tasks){
+       mp[ch-'A']++;
+       }
+       sort(mp.begin(),mp.end()); 
+       int maxFreq = mp[25];
+       int gaddhe = maxFreq-1;
+       int idleSlots = n*gaddhe;
+
+       for(int i=24;i>=0;i--){
+        idleSlots -= min(mp[i],gaddhe);
+       }
+       if(idleSlots>0){
+        return tasks.size()+ idleSlots;
+       }
+       return tasks.size();
+    }
+};
