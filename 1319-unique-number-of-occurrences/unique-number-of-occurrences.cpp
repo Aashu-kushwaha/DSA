987cf9c482e0323ae005b62f0c438d979154c235
Aashu-kushwaha <167160987+Aashu-kushwaha@users.noreply.github.com>
2025-08-30 12:17:15 +0530

    Time: 4 ms (4.96%) | Memory: 12 MB (31.89%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        int i = 0;
-        sort(arr.begin(),arr.end());
-        vector<int> ans;
-        while (i < arr.size()){
-            int count = 1;
-            for (int j = i+1; j< arr.size(); j++){
-                if (arr[i] == arr[j])
-                    count++;
-            }
-            ans.push_back(count);
-            i = i + count;
+        int n = arr.size();
+        unordered_map<int,int>mp;
+        for(int &i:arr){
+            mp[i]++;
         }
-        sort(ans.begin(),ans.end());
-        for (int i = 0; i < ans.size() - 1; i++){
-            if (ans[i] == ans [i+1])
-                return false;
+        vector<int>freq;
+        for(auto i: mp){
+         freq.push_back(i.second);
+        }
+
+        sort(freq.begin(),freq.end());
+        for(int j=0;j<freq.size()-1;j++){
+            if(freq[j]==freq[j+1])
+            return false;
         }
         return true;
     }
