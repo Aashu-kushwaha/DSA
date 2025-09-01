05e3b105e89d250a43ab94aa4ec157960799bdc5
Aashu-kushwaha <167160987+Aashu-kushwaha@users.noreply.github.com>
2025-09-01 11:55:27 +0530

    Time: 51 ms (71.67%) | Memory: 38.1 MB (82.45%) - LeetSync

--- 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.cpp
+++ 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     int maxDistance(string s, int k) {
-        int ans = 0;
-        int north = 0, south = 0, east = 0, west = 0;
-        
-        for (int i = 0; i < s.size(); i++) {
-            char c = s[i];
-            if (c == 'N') north++;
-            else if (c == 'S') south++;
-            else if (c == 'E') east++;
-            else if (c == 'W') west++;
-            
-            int x = abs(north - south);
-            int y = abs(east - west);
-            int MD = x + y;
-            int dis = MD + min(2 * k, i + 1 - MD);
-            ans = max(ans, dis);
+        int maxMd= 0;
+        int east = 0;
+        int south = 0;
+        int north = 0;
+        int west = 0;
+
+        for(int i =0 ; i<s.length() ;i++){
+            if(s[i]=='E') east++;
+            if(s[i]=='W') west++;
+            if(s[i]=='N') north++;
+            if(s[i]=='S') south++;
+
+            int currentMD = abs(east-west)+abs(north-south);
+            int steps = i+1 ;
+            int wasted = steps - currentMD ;
+
+            int extra = 0; 
+            if(wasted!=0){
+                extra = min(2*k , wasted);
+            } 
+            int finalCurrentMd = currentMD + extra ;
+            maxMd = max(finalCurrentMd , maxMd);
         }
-        
-        return ans;
+        return maxMd ;
     }
 };
