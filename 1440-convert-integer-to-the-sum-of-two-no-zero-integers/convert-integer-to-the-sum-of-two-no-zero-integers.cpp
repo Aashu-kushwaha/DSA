97b2181b3201c5f907e30f1f0e491419ad9afa3f
Aashu-kushwaha <167160987+Aashu-kushwaha@users.noreply.github.com>
2025-09-17 10:50:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (61.98%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    vector<int> getNoZeroIntegers(int n) {
-        for (int i = 1; i <= n; i++) {
-            int left = i;
-            int right = n - i;
-            if (!containsZero(left) && !containsZero(right)) {
-                return {left, right};
-            }
+bool check(int n){
+    while(n>1){
+        if(n%10==0){
+            return false;
         }
-        return {-1, -1};
+        n/=10;
     }
-
-    bool containsZero(int n) {
-        while (n > 0) {
-            if (n % 10 == 0) return true;
-            n /= 10;
-        }
-        return false;
+    return true;
+}
+    vector<int> getNoZeroIntegers(int n) {
+       for(int a=1;a<=n-1;a++){
+       int b= n-a;
+       if(check(a) && check(b)){
+        return {a,b};
+       }
+       }
+       return {-1,-1};
     }
 };
